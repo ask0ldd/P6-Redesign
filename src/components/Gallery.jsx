019fd17ef15a963cc4e3c87fd60e1206b1f3a496
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-02-18 10:08:50 +0100

    likes on working but not persistents on page switching

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,10 +6,13 @@ import { useState } from 'react'
 
 const Gallery = (props) => {
 
-    const [likes, setLikes] = useState([])
+    const [likesList, setLikes] = useState(['c67ab8a7'])
 
     const addLike = (rentalId) => {
-        setLikes([...likes].push(rentalId))
+        let likesListCopy = [...likesList]
+        likesListCopy.push(rentalId)
+        setLikes(likesListCopy)
+        console.log(likesList)
     }
 
     return (
@@ -17,7 +20,7 @@ const Gallery = (props) => {
         {
             (props.dataset && /* if datas got fetched */
                 props.dataset.map((logement) => (<Link className="anchor" to={"hebergement/"+logement.id} key={logement.id}>
-                    <ImmoCard addLike={addLike} liked="false" title={logement.title} cover={logement.cover} rentalId={logement.id}/></Link>)))
+                    <ImmoCard addLike={addLike} likes={likesList} title={logement.title} cover={logement.cover} rentalId={logement.id}/></Link>)))
             || ((props.error && !props.loadingState) && /* !props.loadingState > don't show the error div when no real error, ie : it's only loading */
                 <ErrorBox />)}
     </section>
--- src/components/ImmoCard.jsx
+++ src/components/ImmoCard.jsx
@@ -2,11 +2,16 @@ import '../styles/ImmoCard.css'
 //import { PropTypes } from 'prop-types'
 
 const ImmoCard = (props) => {
+
+    /*const likeClicking = (rentalId) => {
+
+    }*/
+
     return (
     <article className="immocard">
         <img className='mainImg' src={props.cover} alt="Appartement cosy"/>
         <p>{props.title}</p>
-        <img className="favicon" src={props.liked==="false" ? "favoutline.svg" : "favfull.svg"}/>
+        <img className="favicon" src={props.likes?.includes(props.rentalId) ? "favfull.svg" : "favoutline.svg"} onClick={(e) => {e.preventDefault(); e.stopPropagation(); props.addLike(props.rentalId)}}/>
     </article>
     )
 }
